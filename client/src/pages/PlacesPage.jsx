85d3f1e4a0badbc817b8324da0ffed99e93b9aaf
Kushagra-23 <kshgrkumar@gmail.com>
2024-02-25 22:00:47 +0530

    Add new place form and upload by link api done

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { Perks } from "../Perks";
+import axios from "axios";
 
 export const PlacesPage = () => {
   const { action } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  const inputHeader = (text) => {
+    return <h2 className="text-2xl mt-4">{text}</h2>;
+  };
+
+  const inputDescription = (text) => {
+    return <p className="text-gray-500 text-sm">{text}</p>;
+  };
+
+  const preInput = (header, description) => {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  };
+
+  const addPhotoByLink = async(e) => {
+    e.preventDefault();
+    const { data: filename } = await axios.post('/upload-by-link' ,{link: photoLink})
+    setAddedPhotos(prev => {
+      return [...prev, filename]
+    });
+    setPhotoLink('');
+  };
+
   return (
     <div>
       {action !== "new" && (
@@ -29,24 +69,39 @@ export const PlacesPage = () => {
       {action === "new" && (
         <div>
           <form>
-            <h2 className="text-2xl mt-4">Title</h2>
-            <p className="text-gray-500 text-sm">Title for your place</p>
+            {preInput("Title", "Title for your place")}
             <input
               type="text"
               placeholder="title, for example: My lovely apt"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
-            <h2 className="text-2xl mt-4">Address</h2>
-            <p className="text-gray-500 text-sm">Address to this place</p>
-            <input type="text" placeholder="address" />
-            <h2 className="text-2xl mt-4">Photos</h2>
-            <p className="text-gray-500 text-sm">more=better</p>
+            {preInput("Address", "Address to this place")}
+            <input
+              type="text"
+              placeholder="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            {preInput("Photos", "more=better")}
             <div className="flex gap-2">
-              <input type="text" placeholder="Add using a link ....jpg" />
-              <button className="bg-gray-200 px-5 rounded-2xl">
+              <input
+                type="text"
+                placeholder="Add using a link ....jpg"
+                value={photoLink}
+                onChange={(e) => setPhotoLink(e.target.value)}
+              />
+              <button className="bg-gray-200 px-5 rounded-2xl" onClick={addPhotoByLink}>
                 Add&nbsp;photo
               </button>
             </div>
             <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              {addedPhotos.length > 0 && addedPhotos.map(link => (
+              <div>
+                  {link}
+                </div>
+                )
+              )}
               <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -64,6 +119,50 @@ export const PlacesPage = () => {
                 </svg>
                 Upload
               </button>
+            </div>
+            {preInput("Description", "Description of the place")}
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            {preInput("Perks", "Select all the perks of your place")}
+            <Perks selected={perks} onChange={setPerks} />
+            {preInput("Extra Info", "house rules, etc")}
+            <textarea
+              value={extraInfo}
+              onChange={(e) => setExtraInfo(e.target.value)}
+            />
+            {preInput("Check in & out times", "Add check in and out times")}
+            <div className="grid gap-2 sm:grid-cols-3">
+              <div>
+                <h3 className="mt-2 -mb-1">Check in time</h3>
+                <input
+                  type="text"
+                  placeholder="14:00"
+                  value={checkIn}
+                  onChange={(e) => setCheckIn(e.target.value)}
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Check out time</h3>
+                <input
+                  type="text"
+                  placeholder="11:00"
+                  value={checkOut}
+                  onChange={(e) => setCheckOut(e.target.value)}
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Max no of guests</h3>
+                <input
+                  type="number"
+                  value={maxGuests}
+                  onChange={(e) => setMaxGuests(e.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <button className="primary my-4">Save</button>
             </div>
           </form>
         </div>
